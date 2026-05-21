3a9dc3211dc1aae11b844e0b13d240b00fdd59a6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-05-21 16:14:42 +0100

    feat: updated featured project order so that workout app is first in the list

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -30,28 +30,6 @@ import postyApp from "../assets/posty_app.webp";
 const projects = {
   featured: [
     {
-      title: "Posty CMS App",
-      image: postyApp,
-      links: {
-        demo: ["https://posts-headless-cms.pages.dev/"],
-        source: ["https://github.com/AndrewAttemptsCode/posts-headless-cms"],
-      },
-      description: [
-        "Posty is a headless CMS web app built to showcase front-end skills — including React, TypeScript, client-side routing, API integration, and responsive, accessible design.",
-        "It displays the latest posts from a Contentful API and lets users navigate to individual articles. Selecting a post uses client-side routing, while direct URL slugs dynamically fetch and render the correct content.",
-        "The app demonstrates modern front-end architecture with a decoupled data flow: API → custom hooks → components. Tailwind CSS ensures responsive, accessible, and maintainable interfaces."
-      ],
-      techstack: {
-        frontend: ["HTML", "CSS", "JavaScript", "React", "Tailwind CSS"],
-        backend: [],
-        buildTools: ["TypeScript", "Vite"],
-        testing: [],
-        versionControl: ["Git", "GitHub"],
-        hosting: ["Cloudflare Pages"],
-        tools: ["VS Code", "Prettier", "Contentful"],
-      },
-    },
-    {
       title: "Workout App",
       image: workoutApp,
       links: {
@@ -71,6 +49,28 @@ const projects = {
         versionControl: ["Git", "GitHub"],
         hosting: ["Cloudflare Pages"],
         tools: ["VS Code"],
+      },
+    },
+    {
+      title: "Posty CMS App",
+      image: postyApp,
+      links: {
+        demo: ["https://posts-headless-cms.pages.dev/"],
+        source: ["https://github.com/AndrewAttemptsCode/posts-headless-cms"],
+      },
+      description: [
+        "Posty is a headless CMS web app built to showcase front-end skills — including React, TypeScript, client-side routing, API integration, and responsive, accessible design.",
+        "It displays the latest posts from a Contentful API and lets users navigate to individual articles. Selecting a post uses client-side routing, while direct URL slugs dynamically fetch and render the correct content.",
+        "The app demonstrates modern front-end architecture with a decoupled data flow: API → custom hooks → components. Tailwind CSS ensures responsive, accessible, and maintainable interfaces."
+      ],
+      techstack: {
+        frontend: ["HTML", "CSS", "JavaScript", "React", "Tailwind CSS"],
+        backend: [],
+        buildTools: ["TypeScript", "Vite"],
+        testing: [],
+        versionControl: ["Git", "GitHub"],
+        hosting: ["Cloudflare Pages"],
+        tools: ["VS Code", "Prettier", "Contentful"],
       },
     },
     {
